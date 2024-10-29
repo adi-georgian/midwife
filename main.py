42a7ea000dfe6bb73d653bd061e037c8701c27bb
Aditya Shukla <adityashukzy@gmail.com>
2024-10-29 21:19:38 +0530

    Incremental Fixes

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from utils import visualize_tree
-from components import Thesis, AntiThesis, Aspect
+from visualize import build_tree
+from components import Thesis, Aspect
 
-thesis = Thesis(OneLiner="Unicorns are real.")
-anithesis = AntiThesis(OneLiner="Unicorns are not real.")
-aspects = \
-[
-    Aspect(title='XYZ', content=aspect_content, parent=thesis) for aspect_content in 
+if __name__ == "__main__":
+    thesis = Thesis(content="Unicorns are real.")
+
+    aspects = \
     [
         "What do you define as 'real'?",
         "Can you support your claim with evidence or examples?",
     ]
-]
 
-visualize_tree(thesis)
+    for aspect in aspects:
+        thesis.add_aspect(aspect)
+
+    aspects = \
+    [
+        "Is 'real' defined as perceptible?",
+        "Can what is 'real' be different for two people?",
+    ]
+
+    for aspect in aspects:
+        thesis.get_aspect("What do you define as 'real'?").add_aspect(aspect)
+
+    aspects = \
+    [
+        "Is it not impossible to prove non-existence",
+        "Why do we need empirical examples?",
+    ]
+
+    for aspect in aspects:
+        thesis.get_aspect("Can you support your claim with evidence or examples?").add_aspect(aspect)
+
+    tree = build_tree(thesis)
